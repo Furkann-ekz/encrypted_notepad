847c5c292d61650e18873be69ae843195a3f4b21
Furkan <123004647+Furkan1071@users.noreply.github.com>
2024-01-10 14:59:26 +0300

    Add files via upload

--- library.h
+++ library.h
@@ -11,7 +11,7 @@
 # include <fcntl.h>
 
 void	ft_closer(unsigned char *str, int yazilacak_dosya, int okunacak_dosya, int pss);
-
+void	close_files(int yazilacak_dosya, int okunacak_dosya, int pss);
 
 unsigned char	*ft_encryption(unsigned char *str);
 unsigned char	*ft_decryption(unsigned char *str);
@@ -22,9 +22,10 @@ unsigned char	*ft_get_read(int fd, unsigned char *str);
 int		ft_strcmp(unsigned char *s1, unsigned char *s2);
 int		ft_strncmp(unsigned char *s1, unsigned char *s2, int size);
 int		ft_error(unsigned char *str, int secim);
-int		ft_secim(unsigned char *str, int secim);
+int		ft_choose(unsigned char *str, int secim);
+int		pss_creator(int pss, unsigned char *s, unsigned char *temp, unsigned char *str);
+int		pss_error(char *str, char *temp);
 
 size_t	ft_strlen(unsigned char *str);
-
 
 #endif
